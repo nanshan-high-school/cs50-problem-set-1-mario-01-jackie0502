9a1e18973b068322be4d63d304442424fccb9cc9
jackie0502 <54939859+jackie0502@users.noreply.github.com>
2019-12-02 22:32:04 +0800

    Create 1121作業.cpp
    第一版
    不知道如何在>8時重來
    因此先設成錯誤

---
+++ 1121作業.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int high = 0;
+    cout << "請輸入高度:";
+    cin >> high;
+    if(high == 1){
+      cout << "#\n";
+    }else if(high == 2){
+      cout << "#\n##";
+    }else if(high == 3){
+      cout << "#\n##\n###";
+    }else if(high == 4){
+      cout << "#\n##\n###\n####";
+    }else if(high == 5){
+      cout << "#\n##\n###\n####\n#####";
+    }else if(high == 6){
+      cout << "#\n##\n###\n####\n#####\n######";
+    }else if(high == 7){
+      cout << "#\n##\n###\n####\n#####\n######\n#######";
+    }else if(high == 8){
+      cout << "#\n##\n###\n####\n#####\n######\n#######\n########";
+    }else{cout << "錯誤，請輸入小於8的值";
+    }
+} 
